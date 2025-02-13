5457599ffeb41ed4a9be8e145396e7b79234117d
Orion Rawlings <orionrawlings@gmail.com>
2025-02-13 11:55:13 -0700

    Deleted the login html and converted it to a react component.

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import './login.css'
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main>
+  <h1>Welcome To Your Centralized Reviews</h1>
+  <form method="get" action="index.html">
+    <div>
+      <span>@</span>
+      <input type="text" placeholder="your@email.com" />
+    </div>
+    <div>
+      <span>🔒</span>
+      <input type="password" placeholder="password" />
+    </div>
+    <button className="site_button" type="submit">Login</button>
+    <button className="site_button" type="submit">Create</button>
+  </form>
+
+  <div>
+    <a href="review.html">
+      <button className="review_button" type="button">Review Anime</button>
+    </a>
+    <a href="review.html">
+      <button className="review_button" type="button">Review Games</button>
+    </a>
+  </div>
+
+  <div>
+    <a href="charts.html">
+      <button type="button">
+        <img src="./image_library/top_charts.png" alt="Top Charts" style={{ width: "700px", height: "300px" }} />
+      </button>
+    </a>
+  </div>
+</main>
   );
 }
