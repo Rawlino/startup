54e92d9727d403d6bc7c2ba7910976a6dcdf148c
Orion Rawlings <orionrawlings@gmail.com>
2025-02-13 11:51:04 -0700

    Deleted charts html and convverted it to a component.

--- src/charts/charts.jsx
+++ src/charts/charts.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import './charts.css'
 
 export function Charts() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>charts displayed here</div>
-    </main>
+    <main>
+    <h1>Who's Cooking Right Now?</h1>
+    <div>
+      <div className="tab">
+          <button className="tablinks" style={{width: "33.3%"}}>Games</button>
+          <button className="tablinks" style={{width: "33.3%"}}>Anime</button>
+          <button className="tablinks" style={{width: "33.3%"}}>Both</button>
+        </div>
+        <div>
+          <h3>Games</h3>
+          <p>This is a placeholder for including top charts</p>
+        </div>
+        <div>
+          <h3>Anime</h3>
+          <p>This is a placeholder for including top charts</p>
+        </div>
+        <div>
+          <h3>Both</h3>
+          <p>This is a placeholder for including top charts</p>
+        </div>
+    </div>
+  </main>
   );
 }
