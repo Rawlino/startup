9ee8471c086971d8b7ce7b711eb91dd7978073b3
Orion Rawlings <orionrawlings@gmail.com>
2025-02-12 21:20:41 -0700

    Created view components.

---
+++ src/charts/charts.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Charts() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>charts displayed here</div>
+    </main>
+  );
+}
