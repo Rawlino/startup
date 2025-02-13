f93add965e3fde147aed4826f92c99bf57763553
Orion Rawlings <orionrawlings@gmail.com>
2025-02-12 21:29:25 -0700

    Created the router.

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,52 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Charts } from './charts/charts';
+import { Review } from './review/review';
+
 
 
 export default function App() {
   return(
-    <div>
-        <header>
-          <h1 className="header">AniGameReviews<sup>&reg;</sup></h1>
-          <h3 className="username"><img className="username" src="/image_library/profile_pic.png" style={{ width: "100px", height: "100px" }} />Username</h3>
-          <nav>
-            <menu className="topnav">
-              <a href="index.html">Home</a>
-              <a href="review.html">Review</a>
-              <a href="charts.html">Charts</a>
-            </menu>
-          </nav>
-          <hr />
-        </header>
-    
-        <main>App components go here</main>
-    
-        <footer>
-          <hr />
-          <span className="text-reset">Author:</span>
-          <br />
-          <h5><a href="https://github.com/Rawlino/startup">Orion's GitHub</a></h5>
-          <div>
-            <button className="site_button" type="button">Give me a duck please. Placeholder for 3rd party service calls.</button>
-          </div>
-        </footer>
-        <div className="parallax"/>
-    </div>
+    <BrowserRouter>
+        <div>
+            <header>
+              <h1 className="header">AniGameReviews<sup>&reg;</sup></h1>
+              <h3 className="username"><img className="username" src="/image_library/profile_pic.png" style={{ width: "100px", height: "100px" }} />Username</h3>
+              <nav>
+                <menu className="topnav">
+                  <NavLink to="login">Home</NavLink>
+                  <NavLink to="review">Review</NavLink>
+                  <NavLink to="charts">Charts</NavLink>
+                </menu>
+              </nav>
+              <hr />
+            </header>
+
+            <Routes>
+             <Route path='/login' element={<Login />} exact />
+             <Route path='/review' element={<Review />} />
+             <Route path='/charts' element={<Charts />} />
+             <Route path='*' element={<NotFound />} />
+            </Routes>
+
+            <footer>
+              <hr />
+              <span className="text-reset">Author:</span>
+              <br />
+              <h5><a href="https://github.com/Rawlino/startup">Orion's GitHub</a></h5>
+              <div>
+                <button className="site_button" type="button">Give me a duck please. Placeholder for 3rd party service calls.</button>
+              </div>
+            </footer>
+            <div className="parallax"/>
+        </div>
+    </BrowserRouter>
   ); 
+}
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
