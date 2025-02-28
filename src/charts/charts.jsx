ee56e506c5920ec7a2f989519719d6e1f05db9a9
Orion Rawlings <orionrawlings@gmail.com>
2025-02-27 17:19:23 -0700

    Haven't commit in a while. Tried getting login figured out and wasn 't able to. So deleted a lot and added a lot from there. Then deleted everything cause I thought I was going in the wrong direction. Then moved over to the charts page and finished how I want the tabs to work. Now I just need to figure out login and storing user reviews after that.

--- src/charts/charts.jsx
+++ src/charts/charts.jsx
@@ -1,29 +1,67 @@
 import React from 'react';
-import './charts.css'
+import './charts.css';
+
+function openCtg(evt, ctgName) {
+  
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  
+  document.getElementById(ctgName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+}
 
 export function Charts() {
   return (
     <main>
-    <h1>Who's Cooking Right Now?</h1>
-    <div>
-      <div className="tab">
-          <button className="tablinks" style={{width: "33.3%"}}>Games</button>
-          <button className="tablinks" style={{width: "33.3%"}}>Anime</button>
-          <button className="tablinks" style={{width: "33.3%"}}>Both</button>
+      <h1>Who's Cooking Right Now?</h1>
+      <div>
+        <div className="tab">
+          <button
+            className="tablinks"
+            onClick={(evt) => openCtg(evt, 'Games')}
+            style={{ width: "33.3%" }}
+            id="defaultOpen"
+          >
+            Games
+          </button>
+          <button
+            className="tablinks"
+            onClick={(evt) => openCtg(evt, 'Anime')}
+            style={{ width: "33.3%" }}
+          >
+            Anime
+          </button>
+          <button
+            className="tablinks"
+            onClick={(evt) => openCtg(evt, 'Both')}
+            style={{ width: "33.3%" }}
+          >
+            Both
+          </button>
         </div>
-        <div>
+        <div id="Games" className="tabcontent">
           <h3>Games</h3>
-          <p>This is a placeholder for including top charts</p>
+          <p>This is a placeholder for including top charts (users reviews will appear here)</p>
         </div>
-        <div>
+        <div id="Anime" className="tabcontent">
           <h3>Anime</h3>
-          <p>This is a placeholder for including top charts</p>
+          <p>This is a placeholder for including top charts (users reviews will appear here)</p>
         </div>
-        <div>
+        <div id="Both" className="tabcontent">
           <h3>Both</h3>
-          <p>This is a placeholder for including top charts</p>
+          <p>This is a placeholder for including top charts (users reviews will appear here)</p>
         </div>
-    </div>
-  </main>
+      </div>
+    </main>
   );
 }
