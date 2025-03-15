1ac17e15d83b68652587693d0df2faf85d58b23e
Orion Rawlings <orionrawlings@gmail.com>
2025-03-15 10:43:53 -0600

    Updated authenticated.jsx to call endpoints.

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -9,10 +9,18 @@ export function Authenticated(props) {
   const navigate = useNavigate();
 
   function logout() {
-    localStorage.removeItem('userName');
-    props.onLogout();
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    })
+      .catch(() => {
+        // Logout failed. Assuming offline
+      })
+      .finally(() => {
+        localStorage.removeItem('userName');
+        props.onLogout();
+      });
   }
-
+  
   return (
     <div>
       <div className='playerName'>Success! What would you like to do?</div>
